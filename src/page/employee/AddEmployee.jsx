647e9574aee3e51d4214fb08a4566ef55baeb799
Mike <s33165309000@gmail.com>
2023-08-03 22:43:53 +0800

    [feat] addEmployee

--- src/page/employee/AddEmployee.jsx
+++ src/page/employee/AddEmployee.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import auth from '../../auth/auth';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { API_URL } from '../../utils/config';
 
 const AddEmployee = () => {
-    auth();
+    // auth();
     const [input, setInput] = useState({
         account: '',
         password: '',
@@ -10,23 +13,69 @@ const AddEmployee = () => {
         name: '',
         salary: '',
     });
-  function handleChange() {
-      setInput()
+    function handleChange(e) {
+        setInput({ ...input, [e.target.id]: e.target.value });
     }
+    async function handleSubmit() {
+        if (
+            input.account == '' ||
+            input.name == '' ||
+            input.password == '' ||
+            input.permission == '' ||
+            input.salary == ''
+        ) {
+            toast.error('有欄位未填寫', {
+                position: 'top-center',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                theme: 'dark',
+            });
+            return false;
+        }
+        let result = await axios.post(`${API_URL}/addEmployee`, input);
+        toast.error(result.data, {
+            position: 'top-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: 'dark',
+        });
+    }
+    function handleClear() {
+        setInput({ account: '', password: '', permission: '', name: '', salary: '' });
+    }
+    console.log(input);
     return (
         <div className="w-full h-[calc(100vh-48px)] flex justify-center items-center">
-            <div className="w-2/3 h-2/3 rounded-3xl border px-5 py-14 grid">
+            <div className="w-full mx-2 sm:w-2/3 h-2/3 rounded-3xl border px-5 py-14 grid">
                 <div>
                     <label htmlFor="">帳號 : </label>
-                    <input type="text" className="bg-white text-[#444] border" />
+                    <input
+                        type="text"
+                        className="bg-white text-[#444] border"
+                        id="account"
+                        value={input.account}
+                        onChange={(e) => handleChange(e)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="">密碼 : </label>
-                    <input type="text" className="bg-white text-[#444] border" />
+                    <input
+                        type="text"
+                        className="bg-white text-[#444] border"
+                        id="password"
+                        value={input.password}
+                        onChange={(e) => handleChange(e)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="">權限 : </label>
-                    <select name="" id="" className="w-44 border">
+                    <select name="" id="permission" className="w-44 border" value={input.permission} onChange={(e) => handleChange(e)}>
                         <option value="">請選擇</option>
                         <option value="1">會計</option>
                         <option value="2">長照</option>
@@ -34,11 +83,31 @@ const AddEmployee = () => {
                 </div>
                 <div>
                     <label htmlFor="">姓名 : </label>
-                    <input type="text" className="bg-white text-[#444] border" />
+                    <input
+                        type="text"
+                        className="bg-white text-[#444] border"
+                        id="name"
+                        value={input.name}
+                        onChange={(e) => handleChange(e)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="">時薪 : </label>
-                    <input type="text" className="bg-white text-[#444] border" />
+                    <input
+                        type="text"
+                        className="bg-white text-[#444] border"
+                        id="salary"
+                        value={input.salary}
+                        onChange={(e) => handleChange(e)}
+                    />
+                </div>
+                <div className="flex justify-center items-center gap-10">
+                    <div className="bg-green-400 py-2 px-4 rounded" onClick={handleSubmit}>
+                        儲存
+                    </div>
+                    <div className="bg-red-500 py-2 px-4 rounded" onClick={handleClear}>
+                        清除
+                    </div>
                 </div>
             </div>
         </div>
