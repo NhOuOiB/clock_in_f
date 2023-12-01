d9b11a298e5c1e49eb8b6082dadcfecba23226ff
mike.chang <s33165309000@gmail.com>
2023-12-01 13:16:03 +0800

    [feat] localstorage individual_id

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 
 const Login = () => {
     const [login, setLogin] = useState({
+        individual_id: 'test',
         account: 'mike',
         password: '1234',
     });
@@ -14,14 +15,14 @@ const Login = () => {
     function handleChange(e) {
         setLogin({ ...login, [e.target.name]: e.target.value });
     }
-    async function handleLogin() {
+  async function handleLogin() {
         try {
             let res = await axios.post(`${API_URL}/login`, login, {
                 withCredentials: true,
             });
             localStorage.setItem('permission', res.data.permission);
             localStorage.setItem('userId', res.data.id);
-            console.log(res);
+            localStorage.setItem('individualId', res.data.individual_id);
             if (res.status == 200) {
                 toast.success(res.data.message, {
                     position: 'top-center',
@@ -34,9 +35,9 @@ const Login = () => {
                 });
             }
             if (res.data.permission == 1) {
-                navigate('/checkRecord');
+                navigate('/clockRecord');
             } else {
-                navigate('/check');
+                navigate('/clock');
             }
         } catch (err) {
             toast.error(err.response.data.message, {
@@ -55,31 +56,35 @@ const Login = () => {
     }, []);
 
     return (
-        <div className="flex justify-center items-center h-full">
-            <div className=" w-44 flex flex-col">
-                <label htmlFor="account" className="text-left">
-                    Account
-                </label>
-                <input type="text" id="account" name="account" onChange={handleChange} />
-                <label htmlFor="password" className="text-left">
-                    Password
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={handleChange}
-                    onKeyDown={(e) => {
-                        if (e.keyCode == 13) {
-                            handleLogin(e);
-                        }
-                    }}
-                />
-                <div className="mt-6 rounded bg-slate-600 text-white" onClick={(e) => handleLogin(e)}>
-                    登入
-                </div>
-            </div>
+      <div className="flex justify-center items-center h-full">
+        <div className=" w-44 flex flex-col">
+          <label htmlFor="individual_id" className="text-left">
+            個案代號
+          </label>
+          <input type="text" id="individual_id" name="individual_id" onChange={handleChange} />
+          <label htmlFor="account" className="text-left">
+            帳號
+          </label>
+          <input type="text" id="account" name="account" onChange={handleChange} />
+          <label htmlFor="password" className="text-left">
+            密碼
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              if (e.keyCode == 13) {
+                handleLogin(e);
+              }
+            }}
+          />
+          <div className="mt-6 rounded bg-slate-600 text-white" onClick={(e) => handleLogin(e)}>
+            登入
+          </div>
         </div>
+      </div>
     );
 };
 
