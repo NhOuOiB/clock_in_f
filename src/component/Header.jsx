478a96659e0c20f306803c74211db5ad9b88ef3f
mike.chang <s33165309000@gmail.com>
2024-04-17 13:59:05 +0800

    [fix] remove toast import

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 const Header = () => {
   const navigate = useNavigate();
